714c15c447876a9d72d3bcb9aaddd5a82986ab9b
Robin Richardson <raarichardson@gmail.com>
2021-08-19 13:55:40 +0200

    Add some documentation of returned dict for get_profile

---
+++ niceday_client/tests/test_niceday_client.py
@@ -0,0 +1,9 @@
+from niceday_client import NicedayClient
+
+def test_get_profile():
+    client = NicedayClient()
+    existing_user_id = 38527
+    profile = client.get_profile(existing_user_id)
+    assert isinstance(profile, dict)
+    assert 'id' in profile
+    assert 'userProfile' in profile
