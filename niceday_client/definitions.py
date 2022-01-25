534aa05f64b90d543fce1d18847a649b1c2583ed
Robin Richardson <raarichardson@gmail.com>
2022-01-25 16:30:51 +0100

    Use Int enums to avoid serialization problem

--- niceday_client/definitions.py
+++ niceday_client/definitions.py
@@ -1,6 +1,6 @@
-from enum import Enum
+from enum import IntEnum
 
 USER_PROFILE_KEYS = ['firstName', 'lastName', 'location', 'birthDate', 'gender']
 
-class Tracker(Enum):
-    smoking = 1
+class Tracker(IntEnum):
+    SMOKING = 1
