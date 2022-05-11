c97e0154f4e414d53ee09a363b2dccfaff9d4f17
Walter Baccinelli <w.baccinelli@esciencecenter.nl>
2022-05-11 14:29:42 +0200

    set tracker reminder

--- niceday_client/definitions.py
+++ niceday_client/definitions.py
@@ -1,6 +1,9 @@
-from enum import IntEnum
+from enum import IntEnum, Enum
 
 USER_PROFILE_KEYS = ['firstName', 'lastName', 'location', 'birthDate', 'gender']
 
 class Tracker(IntEnum):
     SMOKING = 1
+
+class TrackerName(Enum):
+    SMOKING = 'tracker_smoking'
